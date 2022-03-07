ac4ba185f82a2743b01f6dbe1cb39f5ab920636e
Subodha Pathiraja <subodha@atomix.com.au>
2022-03-07 23:04:19 +0530

    Resolving storybook @emotion imports issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,25 @@
-const path = require("path");
+const path = require('path')
+const toPath = (_path) => path.join(process.cwd(), _path)
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "features": {
-    "babelModeV7": true,
+  framework: '@storybook/react',
+  features: {
+    babelModeV7: true,
   },
   webpackFinal: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			"@": path.resolve(__dirname, "../src"),
-		};
-		return config;
-	},
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    }
+    return config
+  },
 }
