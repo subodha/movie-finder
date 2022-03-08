ec6fc427bbf898ddac2451cadccb133570d8b1c0
Subodha Pathiraja <subodha@atomix.com.au>
2022-03-08 21:33:30 +0530

    import Custom fonts & Global styles into storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,32 +1,35 @@
-import { ThemeProvider } from "@emotion/react"
-import { withThemes } from "@react-theming/storybook-addon"
-import * as NextImage from "next/image";
+import { ThemeProvider } from '@emotion/react'
+import { addDecorator } from '@storybook/react'
+import * as NextImage from 'next/image'
 
-import {theme} from '../src/styles/theme'
+import { theme } from '../src/styles/theme'
+import globalStyles from '../src/styles/globals.styled'
 
-export const decorators = [
-  withThemes(ThemeProvider, [theme]),
-]
-
+addDecorator((story) => (
+	<ThemeProvider theme={theme}>
+		{globalStyles}
+		{story()}
+	</ThemeProvider>
+))
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 }
 
-const OriginalNextImage = NextImage.default;
+const OriginalNextImage = NextImage.default
 
-Object.defineProperty(NextImage, "default", {
-  configurable: true,
-  value: (props) => (
-    <OriginalNextImage
-      {...props}
-      unoptimized
-      blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
-    />
-  ),
-});
+Object.defineProperty(NextImage, 'default', {
+	configurable: true,
+	value: (props) => (
+		<OriginalNextImage
+			{...props}
+			unoptimized
+			blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
+		/>
+	),
+})
--- src/styles/globals.styled.tsx
+++ src/styles/globals.styled.tsx
@@ -1,27 +1,27 @@
 import { css, Global } from '@emotion/react'
 
 const globalStyles = (
-  <Global
-    styles={css`
-      html,
-      body {
-        min-height: 100%;
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+	<Global
+		styles={css`
+			html,
+			body {
+				min-height: 100%;
+				padding: 0;
+				margin: 0;
+				font-family: 'Rubik', sans-serif, Roboto, Oxygen, Ubuntu, Cantarell,
+					Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+			}
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
+			a {
+				color: inherit;
+				text-decoration: none;
+			}
 
-      * {
-        box-sizing: border-box;
-      }
-    `}
-  />
+			* {
+				box-sizing: border-box;
+			}
+		`}
+	/>
 )
 
 export default globalStyles
