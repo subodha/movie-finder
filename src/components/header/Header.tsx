925eeab71f7e24ca4d0d9d41c467def602d22c73
Subodha Pathiraja <subodha@atomix.com.au>
2022-03-11 00:00:06 +0530

    Adding header elements

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Slider from 'rc-slider'
 
 import { MovieSearchFormProps } from '@/types/movieSearch'
 
-import { HeaderStyled } from './Header.styled'
+import {
+	HeaderStyled,
+	RcSliderStyled,
+	ResultFilterStyled,
+	ButtonGroupeStyled,
+} from './Header.styled'
 
 type HeaderProps = {
 	movieSearch?: MovieSearchFormProps
@@ -11,8 +16,57 @@ type HeaderProps = {
 export const Header = ({ movieSearch }: HeaderProps): JSX.Element => (
 	<HeaderStyled>
 		<div className="wrapper">
-			<div>Search</div>
-			<div>Advanced filter goes here!</div>
+			<div className="title-search">
+				<input type="search" name="title" placeholder="Search by Title" />
+			</div>
+
+			<ResultFilterStyled>
+				<div>
+					<span>Year</span>
+					<RcSliderStyled>
+						<Slider
+							range
+							allowCross={false}
+							defaultValue={[1995, 2020]}
+							draggableTrack
+							min={1970}
+							max={2015}
+							onChange={(value) => console.log(value)}
+						/>
+					</RcSliderStyled>
+				</div>
+
+				<div>
+					<span>Type</span>
+
+					<ButtonGroupeStyled>
+						<label htmlFor="any">
+							<input type="radio" id="any" name="type" value="" />
+							Any
+						</label>
+
+						<br />
+
+						<label htmlFor="movies">
+							<input type="radio" id="movies" name="type" value="Movies" />
+							Movies
+						</label>
+						<br />
+
+						<label htmlFor="series">
+							<input type="radio" id="series" name="type" value="Series" />
+							Series
+						</label>
+						<br />
+
+						<label htmlFor="episodes">
+							<input type="radio" id="episodes" name="type" value="Episodes" />
+							Episodes
+						</label>
+						<br />
+					</ButtonGroupeStyled>
+				</div>
+			</ResultFilterStyled>
 		</div>
 	</HeaderStyled>
 )
--- src/components/header/header.styled.tsx
+++ src/components/header/header.styled.tsx
@@ -11,5 +11,72 @@ export const HeaderStyled = styled.header`
 		align-items: center;
 		display: flex;
 		flex: 1;
+		color: ${(props) => props.theme.palette.common.white};
 	}
+`
+
+export const RcSliderStyled = styled.div`
+	display: flex;
+	width: 120px;
+
+	.rc-slider {
+		position: relative;
+		width: 100%;
+		height: 14px;
+		padding: 8px 0;
+		border-radius: 6px;
+		touch-action: none;
+		box-sizing: border-box;
+
+		* {
+			box-sizing: border-box;
+			-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		}
+
+		&-rail {
+			position: absolute;
+			width: 100%;
+			height: 4px;
+			background-color: #e9e9e9;
+			border-radius: 6px;
+		}
+
+		&-track {
+			position: absolute;
+			height: 4px;
+			background-color: #abe2fb;
+			border-radius: 6px;
+		}
+
+		&-step {
+			position: absolute;
+			width: 100%;
+			height: 4px;
+			background: transparent;
+			pointer-events: none;
+		}
+
+		&-handle {
+			position: absolute;
+			width: 14px;
+			height: 14px;
+			margin-top: -5px;
+			background-color: #fff;
+			border: 2px solid #96dbfa;
+			border-radius: 50%;
+			cursor: pointer;
+			cursor: -webkit-grab;
+			cursor: grab;
+			opacity: 0.8;
+			touch-action: pan-x;
+		}
+	}
+`
+
+export const ResultFilterStyled = styled.div`
+	display: flex;
+`
+
+export const ButtonGroupeStyled = styled.div`
+	display: flex;
 `
