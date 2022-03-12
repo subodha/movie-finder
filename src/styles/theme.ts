4110642bf8b4a5fb12798f988269f391b48ee6c8
Subodha Pathiraja <subodha@atomix.com.au>
2022-03-12 10:49:22 +0530

    Theme improvements

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,100 +1,136 @@
 // Influance with https://mui.com/customization/default-theme/
+
+const palette = {
+	common: {
+		black: '#000',
+		white: '#fff',
+	},
+	primary: {
+		main: '#1976d2',
+		light: '#42a5f5',
+		dark: '#1565c0',
+		contrastText: '#fff',
+	},
+	secondary: {
+		main: '#9c27b0',
+		light: '#ba68c8',
+		dark: '#7b1fa2',
+		contrastText: '#fff',
+	},
+	success: {
+		main: '#2e7d32',
+		light: '#4caf50',
+		dark: '#1b5e20',
+		contrastText: '#fff',
+	},
+	warning: {
+		main: '#ed6c02',
+		light: '#ff9800',
+		dark: '#e65100',
+		contrastText: '#fff',
+	},
+	error: {
+		main: '#d32f2f',
+		light: '#ef5350',
+		dark: '#c62828',
+		contrastText: '#fff',
+	},
+	info: {
+		main: '#0288d1',
+		light: '#03a9f4',
+		dark: '#01579b',
+		contrastText: '#fff',
+	},
+	grey: {
+		50: '#fcfcfc',
+		100: '#ebebeb',
+		200: '#cccccc',
+		300: '#a0a0a0',
+		400: '#8e8e8e',
+		500: '#666666',
+		600: '#505050',
+		700: '#404040',
+		800: '#303030',
+		900: '#212121',
+	},
+	text: {
+		primary: 'rgba(0, 0, 0, 0.87)',
+		secondary: 'rgba(0, 0, 0, 0.6)',
+		disabled: 'rgba(0, 0, 0, 0.87)',
+	},
+	divider: '#cccccc',
+}
+
+const spacing = [
+	'0',
+	'4px',
+	'8px',
+	'12px',
+	'16px',
+	'20px',
+	'24px',
+	'28px',
+	'32px',
+	'36px',
+	'40px',
+	'44px',
+	'48px',
+	'52px',
+	'56px',
+	'60px',
+	'64px',
+]
+
+const borderRadius = {
+	none: 0,
+	xs: '2px',
+	sm: '4px',
+	md: '8px',
+	lg: '16px',
+	xl: '24px',
+	xxl: '32px',
+	xxxl: '64px',
+	round: '50%',
+	full: '999px',
+}
+
+const shadow = {
+	0: 'none',
+	1: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
+}
+
+const typography = {
+	fontWeightLight: 300,
+	fontWeightRegular: 400,
+	fontWeightMedium: 500,
+	fontWeightBold: 700,
+}
+
+const transitions = {
+	easing: {
+		easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+		easeOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+		easeIn: 'cubic-bezier(0.4, 0, 0.2, 1)',
+		sharp: 'cubic-bezier(0.4, 0, 0.2, 1)',
+	},
+	duration: {
+		shortest: 150,
+		shorter: 200,
+		short: 250,
+		standard: 300,
+		complex: 375,
+		enteringScreen: 225,
+		leavingScreen: 195,
+	},
+}
+
 export const theme = {
-	palette: {
-		common: {
-			black: '#000',
-			white: '#fff',
-		},
-		primary: {
-			main: '#1976d2',
-			light: '#42a5f5',
-			dark: '#1565c0',
-			contrastText: '#fff',
-		},
-		secondary: {
-			main: '#9c27b0',
-			light: '#ba68c8',
-			dark: '#7b1fa2',
-			contrastText: '#fff',
-		},
-		success: {
-			main: '#2e7d32',
-			light: '#4caf50',
-			dark: '#1b5e20',
-			contrastText: '#fff',
-		},
-		warning: {
-			main: '#ed6c02',
-			light: '#ff9800',
-			dark: '#e65100',
-			contrastText: '#fff',
-		},
-		error: {
-			main: '#d32f2f',
-			light: '#ef5350',
-			dark: '#c62828',
-			contrastText: '#fff',
-		},
-		info: {
-			main: '#0288d1',
-			light: '#03a9f4',
-			dark: '#01579b',
-			contrastText: '#fff',
-		},
-		grey: {
-			50: '#fafafa',
-			100: '#f4f4f4',
-			200: '#ebebeb',
-			300: '#fafafa',
-			400: '#fafafa',
-			500: '#666666',
-			600: '#8e8e8e',
-			700: '#fafafa',
-			800: '#303030',
-			900: '#303030',
-		},
-		text: {
-			primary: 'rgba(0, 0, 0, 0.87)',
-			secondary: 'rgba(0, 0, 0, 0.6)',
-			disabled: 'rgba(0, 0, 0, 0.87)',
-		},
-		divider: '#bbbbbb',
-	},
-	spacing: [
-		'0',
-		'4px',
-		'8px',
-		'12px',
-		'16px',
-		'20px',
-		'24px',
-		'28px',
-		'32px',
-		'36px',
-		'40px',
-		'44px',
-		'48px',
-		'52px',
-		'56px',
-		'60px',
-		'64px',
-	],
-	borderRadius: {
-		none: 0,
-		xs: '2px',
-		sm: '4px',
-		md: '8px',
-		lg: '16px',
-		xl: '24px',
-		xxl: '32px',
-		xxxl: '64px',
-		round: '50%',
-		full: '999px',
-	},
-	shadow: {
-		0: 'none',
-		1: '0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)',
-	},
+	palette,
+	spacing,
+	borderRadius,
+	shadow,
+	typography,
+	transitions,
 }
 
 export type ThemeTypes = typeof theme
