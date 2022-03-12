e3c30e17880d464ec0c9c8a60e6cfe6c72674de0
Subodha Pathiraja <subodha@atomix.com.au>
2022-03-12 23:02:52 +0530

    Movie detail component initial

--- src/components/movieDetail/MovieDetail.styled.tsx
+++ src/components/movieDetail/MovieDetail.styled.tsx
@@ -1,9 +1,155 @@
 import styled from '@emotion/styled'
 
-// import { breakpoint } from '@/styles/theme'
+import { breakpoint } from '@/styles/theme'
 
 export const MovieDetailStyled = styled.div`
-	position: relative;
-	display: flex;
+	color: ${(props) => props.theme.palette.text.primary};
 	justify-content: center;
+	position: relative;
+	overflow-y: auto;
+	display: flex;
+	position: fixed;
+	background: ${(props) => props.theme.palette.common.white};
+	top: 0;
+	transition: transform ${(props) => props.theme.transitions.duration.short}
+		${(props) => props.theme.transitions.easing.easeInOut};
+	transform: translateX(100%);
+
+	${breakpoint('md')} {
+		position: relative;
+		transform: translateX(0);
+	}
+
+	.movie {
+		padding: ${(props) => props.theme.spacing[5]};
+		display: flex;
+		flex-direction: column;
+
+		header {
+			border-bottom: 1px solid ${(props) => props.theme.palette.divider};
+			padding: ${(props) => props.theme.spacing[5]} 0;
+			display: flex;
+		}
+
+		&-poster {
+			margin-right: ${(props) => props.theme.spacing[5]};
+			// TODO: to check clamp is not working properly
+			// width: clamp(5rem, 25%, 6rem);
+
+			.image-ratio {
+				min-width: 10rem;
+				border-radius: ${(props) => props.theme.borderRadius.sm};
+				padding-bottom: 160%;
+				position: relative;
+				overflow: hidden;
+				display: block;
+			}
+		}
+
+		&-info {
+			justify-content: space-between;
+			flex-direction: column;
+			display: flex;
+			flex: 1;
+
+			h2 {
+				font-weight: ${(props) => props.theme.typography.fontWeightMedium};
+				color: ${(props) => props.theme.palette.text.primaryDark};
+				margin: 0 0 ${(props) => props.theme.spacing[2]};
+				font-size: 2rem;
+			}
+
+			ul {
+				font-weight: ${(props) => props.theme.typography.fontWeightLight};
+				align-items: center;
+				font-size: 1rem;
+				list-style: none;
+				flex-wrap: wrap;
+				display: flex;
+				padding: 0;
+
+				li {
+					padding: ${(props) => props.theme.spacing[1]}
+						${(props) => props.theme.spacing[3]};
+					align-items: center;
+					position: relative;
+					display: flex;
+					white-space: nowrap;
+
+					&:before {
+						align-items: center;
+						position: absolute;
+						line-height: 1px;
+						display: flex;
+						content: '.';
+						height: 1px;
+						left: -2px;
+						top: 10px;
+						font-size: 14px;
+						font-weight: ${(props) => props.theme.typography.fontWeightRegular};
+					}
+
+					&.rated,
+					&.year {
+						&:before {
+							display: none;
+						}
+					}
+
+					&.rated {
+						border: 1px solid ${(props) => props.theme.palette.text.primary};
+						padding: ${(props) => props.theme.spacing[1]}
+							${(props) => props.theme.spacing[2]};
+						border-radius: ${(props) => props.theme.borderRadius.sm};
+						font-size: ${(props) => props.theme.spacing[3]};
+					}
+				}
+			}
+
+			p {
+				font-weight: ${(props) => props.theme.typography.fontWeightLight};
+				font-size: ${(props) => props.theme.spacing[4]};
+				margin-bottom: 0;
+			}
+
+			&-watchlist-container {
+				align-self: flex-end;
+			}
+		}
+
+		&-plot {
+			border-bottom: 1px solid ${(props) => props.theme.palette.divider};
+			font-weight: ${(props) => props.theme.typography.fontWeightLight};
+			font-size: ${(props) => props.theme.spacing[4]};
+			padding: ${(props) => props.theme.spacing[5]} 0;
+			display: flex;
+		}
+
+		&-ratings {
+			font-weight: ${(props) => props.theme.typography.fontWeightLight};
+			padding: ${(props) => props.theme.spacing[5]} 0;
+			justify-content: space-evenly;
+			display: flex;
+
+			span {
+				font-size: ${(props) => props.theme.spacing[3]};
+				text-align: center;
+				width: 100%;
+			}
+
+			&-item {
+				justify-content: center;
+				flex-wrap: wrap;
+				display: flex;
+			}
+
+			.divider {
+				border-right: 1px solid ${(props) => props.theme.palette.divider};
+
+				&:last-child {
+					display: none;
+				}
+			}
+		}
+	}
 `
--- src/components/movieDetail/MovieDetail.tsx
+++ src/components/movieDetail/MovieDetail.tsx
@@ -1,10 +1,103 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 
+import Image from 'next/image'
+
+import { Button } from '../button'
 import { MovieDetailStyled } from './MovieDetail.styled'
 
 // type MovieDetailProps = {}
 
+export const dummyMovieDetails = {
+	Title: 'Batman Begins',
+	Year: '2005',
+	Rated: 'PG-13',
+	Released: '15 Jun 2005',
+	Runtime: '140 min',
+	Genre: 'Action, Crime, Drama',
+	Director: 'Christopher Nolan',
+	Writer: 'Bob Kane, David S. Goyer, Christopher Nolan',
+	Actors: 'Christian Bale, Michael Caine, Ken Watanabe',
+	Plot: 'When his parents are killed, billionaire playboy Bruce Wayne relocates to Asia, where he is mentored by Henri Ducard and Ras Al Ghul in how to fight evil. When learning about the plan to wipe out evil in Gotham City by Ducard, Bruce prevents this plan from getting any further and heads back to his home. Back in his original surroundings, Bruce adopts the image of a bat to strike fear into the criminals and the corrupt as the icon known as "Batman". But it doesn\'t stay quiet for long.',
+	Language: 'English, Mandarin',
+	Country: 'United States, United Kingdom',
+	Awards: 'Nominated for 1 Oscar. 13 wins & 79 nominations total',
+	Poster:
+		'https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+	Ratings: [
+		{
+			Source: 'Internet Movie Database',
+			Value: '8.2/10',
+		},
+		{
+			Source: 'Rotten Tomatoes',
+			Value: '84%',
+		},
+		{
+			Source: 'Metacritic',
+			Value: '70/100',
+		},
+	],
+	Metascore: '70',
+	imdbRating: '8.2',
+	imdbVotes: '1,392,519',
+	imdbID: 'tt0372784',
+	Type: 'movie',
+	DVD: '18 Oct 2005',
+	BoxOffice: '$206,852,432',
+	Production: 'N/A',
+	Website: 'N/A',
+	Response: 'True',
+}
+
 export const MovieDetail = (): JSX.Element => (
-	<MovieDetailStyled>Movie Details goes here...!</MovieDetailStyled>
+	<MovieDetailStyled>
+		<article className="movie">
+			<header>
+				<div className="movie-poster">
+					<div className="image-ratio">
+						<Image
+							alt={`${dummyMovieDetails.Title} poster`}
+							src={dummyMovieDetails.Poster}
+							layout="fill"
+							objectFit="cover"
+							loading="eager"
+						/>
+					</div>
+				</div>
+
+				<div className="movie-info">
+					<div className="movie-info-watchlist-container">
+						<Button label="Watchlist" />
+					</div>
+					<div>
+						<h2>{dummyMovieDetails.Title}</h2>
+						<ul>
+							<li className="rated">{dummyMovieDetails.Rated}</li>
+							<li className="year">{dummyMovieDetails.Year}</li>
+							<li className="genre">{dummyMovieDetails.Genre}</li>
+							<li className="run-time">{dummyMovieDetails.Runtime}</li>
+						</ul>
+						<p>{dummyMovieDetails.Actors}</p>
+					</div>
+				</div>
+			</header>
+
+			<div className="movie-plot">
+				<span>{dummyMovieDetails.Plot}</span>
+			</div>
+
+			<div className="movie-ratings">
+				{dummyMovieDetails.Ratings.map((rate) => (
+					<>
+						<div className="movie-ratings-item">
+							<p>{rate.Value}</p>
+							<span>{rate.Source}</span>
+						</div>
+						<div className="divider" />
+					</>
+				))}
+			</div>
+		</article>
+	</MovieDetailStyled>
 )
